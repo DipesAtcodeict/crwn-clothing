60454bbbafd75e482cf6f04d2c285da11d3798ce
CodeAddictDipesh <45348267+DipesAtcodeict@users.noreply.github.com>
2019-11-18 20:29:10 +0545

    added redux and implmented userReducer and userAction to header and app component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,37 +1,43 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import {auth} from '../../firebase/firebase.utils';
+import { auth } from "../../firebase/firebase.utils";
 
-import './header.styles.scss';
-import {ReactComponent as Logo} from '../../assets/crown.svg';
+import "./header.styles.scss";
+import { ReactComponent as Logo } from "../../assets/crown.svg";
 
+import { connect } from "react-redux";
 
+const Header = ({ currentUser }) => (
+  <div className="header">
+    <Link className="logo-container" to="/">
+      <Logo className="logo" />
+    </Link>
 
-const Header = ({currentUser}) => (
-    <div className='header'>
-        <Link className='logo-container' to="/">
-            <Logo className='logo'/>
-        </Link>
+    <div className="options">
+      <Link className="option" to="/shop">
+        SHOP
+      </Link>
 
-        <div className='options'>
-            <Link className='option' to='/shop'>
-                SHOP
-            </Link>
+      <Link className="option" to="/shop">
+        CONTACT
+      </Link>
 
-            <Link className='option' to='/shop'>
-                CONTACT
-            </Link>
-
-            {
-                currentUser ?
-                <div className='option' onClick={()=>auth.signOut()}>SIGN OUT</div>
-                :
-                <Link className='option' to='/signin'>SIGN IN</Link>
-            }
+      {currentUser ? (
+        <div className="option" onClick={() => auth.signOut()}>
+          SIGN OUT
         </div>
+      ) : (
+        <Link className="option" to="/signin">
+          SIGN IN
+        </Link>
+      )}
     </div>
-)
+  </div>
+);
 
+const mapStateToPros = state => ({
+    currentUser:state.user.currentUser
+})
 
-export default Header;
+export default connect(mapStateToPros)(Header);
